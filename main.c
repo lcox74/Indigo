8ac9d836beee793b0089f6b871ae4245b1c42461
lcox74 <lcox74@outlook.com>
2021-01-05 11:54:44 +1000

    Print Debugging

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include "display_driver.h"
 
 int main(int argc, char *argv[]) {
+    printf("Initialising Display...");
     uint16_t err;
     if ((err = IT8951_init())) {
+        printf("\t[FAILED]\n");
         printf("Error: %d", err);
         return err;
     }
+    printf("\t[DONE]\n");
     
+    printf("Clearing Display\n");
     IT8951_wait_display_ready();
     IT8951_clear_display(0x00); /* White */
 
+    printf("Draw To Display\n");
     IT8951_draw_pixel(0,0,0);
     IT8951_wait_display_ready();
     IT8951_update_display();
 
     // IT8951_clear_display(0xFF); /* Black */
 
+    printf("Clean Display\n");
     IT8951_destroy();
     return 0;
 }
