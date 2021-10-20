1ed3c44844cbf74dd521590403fcbbf886ce213b
lcox74 <lcox74@outlook.com>
2021-10-20 12:09:42 +1000

    seperating api keys

--- main.c
+++ main.c
@@ -1,23 +1,18 @@
 #include "display_driver.h"
 
 int main(int argc, char *argv[]) {
-    printf("Initialising Display\n");
     uint16_t err;
     if ((err = IT8951_init())) {
-        printf("Initialisation Error: %d", err);
+        printf("Display Error: %d", err);
         return err;
     }
 
     IT8951_sys_info info;
-    IT8951_get_system_info(&info);
-    printf("Display (w, h) -> (%u, %u)\n", info.pw, info.ph);
 
-    printf("Clearing Display\n");
     IT8951_wait_display_ready();
     IT8951_clear_display(0xFF); /* White */
 
-    printf("Drawing Image\n");
-    IT8951_draw_jpeg(0, 0, "image.jpg");
+    IT8951_draw_jpeg(0, 0, "./image.jpg");
 
 
     IT8951_wait_display_ready();
--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-import spotipy, requests
+import spotipy, requests, os, json
 from datetime import datetime
 from spotipy.oauth2 import SpotifyOAuth
 from html2image import Html2Image
+from PIL import Image
 
+config_file = open('config.json', 'r')
+config_data = json.load(config_file)
+config_file.close()
 
 # Spotify App
-client_id               = ""
-client_secret           = ""
+client_id               = config_data['spotify_app']['client_id']
+client_secret           = config_data['spotify_app']['client_secret']
 redirect_uri            = "http://example.com/callback"
 scope                   = "user-library-read user-read-currently-playing"
 
 # Open Weather Key
-open_weather_api_key    = ''
-weather_location_id     = ''
+open_weather_api_key    = config_data['open_weather']['api_key']
+weather_location_id     = config_data['open_weather']['locations'][0]['id']
 
 
-def get_current_song() -> (str, str):
+def get_current_song() -> (str, str, str):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
     results = sp.current_user_playing_track()
 
     song_name = results['item']['name']
     artist_name = results['item']['album']['artists'][0]['name']
-
     song_image = results['item']['album']['images'][0]['url']
-    img_data = requests.get(song_image).content
-    with open('album.jpg', 'wb') as handler:
-        handler.write(img_data)
     
-    return (song_name, artist_name)
+    return (song_name, artist_name, song_image)
 
 def get_current_weather_icon(location_id) -> str:
     URL = 'http://api.openweathermap.org/data/2.5/weather'
@@ -40,17 +40,16 @@ def get_current_weather_icon(location_id) -> str:
     r = requests.get(URL, params=PARAMS)
     return r.json()['weather'][0]['icon']
 
-
-
 # Image Values
-song, artist = get_current_song()
+print("Fetching song from Spotify")
+song, artist, img = get_current_song()
 current_time = datetime.now().strftime("%H:%M")
 icon = get_current_weather_icon(weather_location_id)
 
-html_code = """<html style="width: 600px; height: 800px;">
+html_code = """<html style="background-color: #FFF;width: 600px; height: 800px;">
 <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
     <div style="position:absolute; top: 100; left: 140">
-        <img style="width: 320px; height: 320px; box-shadow: 0px 10px 10px #AAA" src="./album.jpg" alt="">
+        <img style="width: 320px; height: 320px; box-shadow: 0px 10px 10px #AAA" src="{}" alt="">
     </div>
     <div style="position: absolute; top: 490px; width: 600px">
         <p style="text-align: center; font-size: 50px; color: #2A2A2A; margin: 0;">{}</p>
@@ -66,8 +65,21 @@ html_code = """<html style="width: 600px; height: 800px;">
         </div>
     </div>
 </body>
-</html>""".format(song, artist, current_time, icon)
+</html>""".format(img, song, artist, current_time, icon)
 
 
+print("Constructing ePaper Graphic")
 hti = Html2Image()
-hti.screenshot(html_str=html_code, save_as='image.jpg', size=(600, 800))
+hti.screenshot(html_str=html_code, save_as='image.png', size=(600, 800))
+
+# Temporarily converting png to jpg
+temp_png = Image.open('image.png')
+temp_png.load()
+
+temp_jpg = Image.new("RGB", temp_png.size, (255, 255, 255))
+temp_jpg.paste(temp_png, mask=temp_png.split()[3])
+temp_jpg.save('image.jpg', 'JPEG', quality=80)
+
+print("Writing to display")
+os.system('sudo ./smartframe.out')
+
