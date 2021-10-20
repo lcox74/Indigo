895d413a408e043799dd34f030d5c94e4333d583
lcox74 <lcox74@outlook.com>
2021-10-21 08:13:09 +1000

    dont refresh when nothing changes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import spotipy, requests, os, json, base64, random
+import spotipy, requests, os, json, base64, random, time
 from datetime import datetime
 from spotipy.oauth2 import SpotifyOAuth
 from html2image import Html2Image
 from PIL import Image
 
-config_file = open('config.json', 'r')
-config_data = json.load(config_file)
-config_file.close()
+with open('config.json', 'r') as f:
+    config_data = json.load(f)
 
 # Spotify App
 client_id               = config_data['spotify_app']['client_id']
@@ -17,6 +16,15 @@ scope                   = "user-library-read user-read-currently-playing"
 # Open Weather Key
 open_weather_api_key    = config_data['open_weather']['api_key']
 weather_location_id     = config_data['open_weather']['locations'][0]['id']
+
+# Last Refresh - Refresh if anything is changed or got to its delay
+last_weather_icon       = config_data['smartframe_app']['last_refresh']['weather_icon']
+last_weather_temp       = config_data['smartframe_app']['last_refresh']['weather_temp']
+last_image              = config_data['smartframe_app']['last_refresh']['image']
+last_song               = config_data['smartframe_app']['last_refresh']['song']
+last_timestamp          = config_data['smartframe_app']['last_refresh']['timestamp']
+refresh_delay           = config_data['smartframe_app']['refresh_delay']
+target_timestamp        = last_timestamp + refresh_delay
 
 # OpenWeather to https://erikflowers.github.io/weather-icons/
 icon_map = {
@@ -53,7 +61,7 @@ def get_current_song() -> (bool, str, str, str):
     
     return (results['is_playing'], song_name, artist_name, song_image)
 
-def get_current_weather_icon(location_id) -> str:
+def get_current_weather(location_id) -> (str, str):
     URL = 'http://api.openweathermap.org/data/2.5/weather'
     PARAMS = {
         'id': location_id,
@@ -62,14 +70,19 @@ def get_current_weather_icon(location_id) -> str:
     }
 
     r = requests.get(URL, params=PARAMS)
+
+    if r.status_code != 200:
+        return ("", "")
+
+    weather_temp = str(round(r.json()['main']['temp'])) + "°C"
     weather_code = r.json()['weather'][0]['icon']
 
     if weather_code in icon_map:
-        return icon_map[weather_code]
+        return (weather_temp, icon_map[weather_code])
     
-    return "wi-cloud"
+    return (weather_temp, "wi-cloud")
 
-def get_html_song(song, artist, img, current_time, weather_icon):
+def get_html_song(song, artist, img, weather_temp, weather_icon):
     return """
     <html style="background-color: #FFF;width: 600px; height: 800px;">
     <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
@@ -91,9 +104,9 @@ def get_html_song(song, artist, img, current_time, weather_icon):
         </div>
     </body>
     </html>
-    """.format(img, song, artist, current_time, icon)
+    """.format(img, song, artist, weather_temp, icon)
 
-def get_html_showcase(current_time, weather_icon):
+def get_html_showcase(weather_temp, weather_icon):
     # Gets random image from the ./imgs directory
     random_file = "./imgs/" + random.choice(os.listdir("./imgs"))
     data = open(random_file, 'rb').read() # read bytes from file
@@ -114,21 +127,33 @@ def get_html_showcase(current_time, weather_icon):
     </div>
 </body>
 </html>
-    """.format(data_base64, current_time, icon)
+    """.format(data_base64, weather_temp, icon)
 
 # Image Values
 print("Fetching song from Spotify")
 playing_song, song, artist, img = get_current_song()
 current_time = datetime.now().strftime("%H:%M")
-icon = get_current_weather_icon(weather_location_id)
+temperature, icon = get_current_weather(weather_location_id)
 
+if (song == last_song and img == last_image and icon == last_weather_icon and temperature == last_weather_temp):
+    if (time.time() < target_timestamp):
+        quit()
+
+config_data['smartframe_app']['last_refresh']['weather_icon'] = icon
+config_data['smartframe_app']['last_refresh']['weather_temp'] = temperature
+config_data['smartframe_app']['last_refresh']['image'] = img
+config_data['smartframe_app']['last_refresh']['song'] = song
+config_data['smartframe_app']['last_refresh']['timestamp'] = time.time()
+
+with open('config.json', 'w') as f:
+    json.dump(config_data, f)
 
 # Determine HTML Code
 html_code = ""
 if playing_song:
-    html_code = get_html_song(song, artist, img, current_time, icon)
+    html_code = get_html_song(song, artist, img, temperature, icon)
 else:
-    html_code = get_html_showcase(current_time, icon)
+    html_code = get_html_showcase(temperature, icon)
 
 print("Constructing ePaper Graphic")
 hti = Html2Image()
